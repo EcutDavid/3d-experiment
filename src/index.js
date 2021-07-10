468bd851f6f70c1a8755c75f80be0688f71462f6
David Guan <davidguan@canva.com>
2021-07-10 16:58:13 +1000

    try line segment mesh

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-function createCubeMesh(x, color) {
+function createCubeLineSegMesh(x, color) {
+  const geometry = new THREE.WireframeGeometry(new THREE.BoxGeometry(1, 1, 1));
+  const material = new THREE.LineBasicMaterial({ color });
+  const cube = new THREE.LineSegments(geometry, material);
+  cube.position.x = x;
+  cube.position.z = -2;
+  return cube;
+}
+function createCubeSolidMesh(x, color) {
   const geometry = new THREE.BoxGeometry(1, 1, 1);
   const material = new THREE.MeshPhongMaterial({ color });
   const cube = new THREE.Mesh(geometry, material);
@@ -10,15 +18,25 @@ function createCubeMesh(x, color) {
 }
 
 const scene = new THREE.Scene();
-const light = new THREE.DirectionalLight(0xffffff, 1);
+const light = new THREE.DirectionalLight(0xffffff, 2);
 light.position.set(4, 2, 4);
 const cubes = [];
-cubes.push(createCubeMesh(-2, 0x3333bb));
-cubes.push(createCubeMesh(0, 0x33bb33));
-cubes.push(createCubeMesh(2, 0xbb3333));
+cubes.push(createCubeSolidMesh(-2, 0x3333bb));
+cubes.push(createCubeSolidMesh(0, 0x33bb33));
+cubes.push(createCubeSolidMesh(2, 0xbb3333));
+cubes.push(createCubeLineSegMesh(-2, 0x3333bb));
+cubes.push(createCubeLineSegMesh(0, 0x33bb33));
+cubes.push(createCubeLineSegMesh(2, 0xbb3333));
 for (const c of cubes) {
   scene.add(c);
 }
+const plane = new THREE.Mesh(
+  new THREE.PlaneGeometry(100, 100),
+  new THREE.MeshPhongMaterial({ color: 0xffffff, side: THREE.DoubleSide })
+);
+plane.rotation.x = Math.PI / 2;
+plane.position.y = -1.2;
+scene.add(plane);
 scene.add(light);
 
 const camera = new THREE.PerspectiveCamera(
@@ -27,7 +45,7 @@ const camera = new THREE.PerspectiveCamera(
   0.1, // near
   1000 // far
 );
-camera.position.z = 5;
+camera.position.z = 8;
 
 const canvas = document.querySelector("#glCanvas");
 const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
@@ -52,7 +70,7 @@ function animate() {
   requestAnimationFrame(animate);
   controls.update();
   for (const c of cubes) {
-    c.rotation.z += 0.01;
+    c.rotation.x += 0.01;
   }
   resizeRendererToDisplaySize(renderer, camera);
   renderer.render(scene, camera);
