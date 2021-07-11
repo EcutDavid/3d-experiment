fd0e1232d669c0bef6d1279a872a1b6d8852441d
David Guan <davidguan@canva.com>
2021-07-11 22:46:35 +1000

    Mug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-function createCubeLineSegMesh(x, color) {
+const inputAreaHeight = 180;
+
+const textureLoader = new THREE.TextureLoader();
+const gltfLoader = new GLTFLoader();
+function createCubeLineSegs(x, color) {
   const geometry = new THREE.WireframeGeometry(new THREE.BoxGeometry(1, 1, 1));
   const material = new THREE.LineBasicMaterial({ color });
   const cube = new THREE.LineSegments(geometry, material);
@@ -9,35 +14,59 @@ function createCubeLineSegMesh(x, color) {
   cube.position.z = -2;
   return cube;
 }
-function createCubeSolidMesh(x, color) {
+
+function createCubeMesh(x, color) {
   const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshPhongMaterial({ color });
+  const material = new THREE.MeshPhongMaterial({
+    color,
+    map: textureLoader.load("./github.png"),
+  });
   const cube = new THREE.Mesh(geometry, material);
   cube.position.x = x;
   return cube;
 }
 
-const scene = new THREE.Scene();
-const light = new THREE.DirectionalLight(0xffffff, 2);
-light.position.set(4, 2, 4);
-const cubes = [];
-cubes.push(createCubeSolidMesh(-2, 0x3333bb));
-cubes.push(createCubeSolidMesh(0, 0x33bb33));
-cubes.push(createCubeSolidMesh(2, 0xbb3333));
-cubes.push(createCubeLineSegMesh(-2, 0x3333bb));
-cubes.push(createCubeLineSegMesh(0, 0x33bb33));
-cubes.push(createCubeLineSegMesh(2, 0xbb3333));
-for (const c of cubes) {
-  scene.add(c);
+function createSphereSegs(x, y, size, color) {
+  const geometry = new THREE.WireframeGeometry(
+    new THREE.SphereGeometry(size, 8, 8)
+  );
+  const material = new THREE.LineBasicMaterial({ color });
+  const sphere = new THREE.LineSegments(geometry, material);
+  sphere.position.x = x;
+  sphere.position.y = y;
+  return sphere;
 }
-const plane = new THREE.Mesh(
-  new THREE.PlaneGeometry(100, 100),
-  new THREE.MeshPhongMaterial({ color: 0xffffff, side: THREE.DoubleSide })
+
+const scene = new THREE.Scene();
+const ambLight = new THREE.AmbientLight(0xffffff, 0.7);
+scene.add(ambLight);
+const dirLight = new THREE.DirectionalLight(0xffffff, 0.5);
+dirLight.position.set(4, 2, 4);
+scene.add(dirLight);
+const objList = [];
+let mugTexture;
+gltfLoader.load(
+  "./mug.gltf",
+
+  (gltf) => {
+    const targetObjs = gltf.scene.children.filter((d) =>
+      ["mug_main", "mug_texture"].includes(d.name)
+    );
+    mugTexture = targetObjs.find((d) => d.name === "mug_texture");
+    textureLoader.load("./github.png", (t) => {
+      mugTexture.material.map = t;
+    });
+    for (const o of targetObjs) {
+      scene.add(o);
+      objList.push(o);
+      // TODO: update the model
+      o.scale.x *= 5;
+      o.scale.y *= 5;
+      o.scale.z *= 5;
+    }
+    scene.add(gltf.scene.children[0]);
+  }
 );
-plane.rotation.x = Math.PI / 2;
-plane.position.y = -1.2;
-scene.add(plane);
-scene.add(light);
 
 const camera = new THREE.PerspectiveCamera(
   75, // FOV
@@ -50,11 +79,11 @@ camera.position.z = 8;
 const canvas = document.querySelector("#glCanvas");
 const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
 const controls = new OrbitControls(camera, renderer.domElement);
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+renderer.setSize(window.innerWidth, window.innerHeight - inputAreaHeight);
 
 function resizeRendererToDisplaySize(renderer, camera) {
-  const { clientWidth: bW, clientHeight: bH } = document.body;
+  let { clientWidth: bW, clientHeight: bH } = document.body;
+  bH -= inputAreaHeight;
   const { clientWidth: cW, clientHeight: cH } = canvas;
   // Assumed that the renderer occupied the whole window.
   if (bW === cW && bH === cH) {
@@ -69,11 +98,21 @@ function resizeRendererToDisplaySize(renderer, camera) {
 function animate() {
   requestAnimationFrame(animate);
   controls.update();
-  for (const c of cubes) {
-    c.rotation.x += 0.01;
+  for (const c of objList) {
+    // c.rotation.z += 0.01;
   }
   resizeRendererToDisplaySize(renderer, camera);
   renderer.render(scene, camera);
 }
 
 animate();
+
+const imageUrlInput = document.querySelector("#imageUrl");
+const submitButton = document.querySelector("#submitButton");
+
+submitButton.addEventListener("click", () => {
+  console.log('?')
+  textureLoader.load(imageUrlInput.value, (t) => {
+    mugTexture.material.map = t;
+  });
+});
